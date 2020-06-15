98c3c8befca757f0315bba00634f65e5c3a02cc0
daochitoan <66459256+daochitoan@users.noreply.github.com>
2020-06-15 16:54:34 +0700

    hci

---
+++ fitness-ui-master/fitness-ui-master/lib/components/next_step.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+
+class NextStep extends StatelessWidget {
+  final String image, title;
+
+  final String rep;
+  NextStep({
+    @required this.image,
+    @required this.title,
+    //@required this.seconds,
+    @required this.rep,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: <Widget>[
+        Container(
+          height: 60.0,
+          width: 60.0,
+          margin: EdgeInsets.only(
+            right: 20.0,
+            bottom: 20.0,
+          ),
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage(
+                this.image,
+              ),
+              fit: BoxFit.cover,
+            ),
+            borderRadius: BorderRadius.circular(15.0),
+          ),
+        ),
+        Container(
+          height: 65.0,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text(
+                this.title,
+                style: TextStyle(
+                  fontSize: 16.0,
+                  color: Colors.black87,
+                ),
+              ),
+              Text(
+                '${this.rep}',
+                style: TextStyle(
+                  fontSize: 14.0,
+                  color: Colors.blueGrey[200],
+                ),
+              )
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
